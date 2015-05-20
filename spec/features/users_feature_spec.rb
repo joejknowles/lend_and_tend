17488c4a5d0ebf705741f05deb7f82fcdf743607
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-05-20 16:33:01 +0100

    Finished Facebook authentication feature with tests

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'User Managament' do
+feature 'User Management' do
 
   context 'User is not signed in' do
 
@@ -42,6 +42,41 @@ feature 'User Managament' do
     it 'should see "Log out" link' do
       visit '/'
       expect(page).to have_link 'Log out'
+    end
+
+  end
+
+  context 'Facebook user management' do
+
+    before do
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new provider: 'facebook', uid: '12345', info: { name: 'Tester', email: 'test@test.com', image: 'http://image.images.com' }
+    end
+
+    it 'when logged out can see "Join with Facebook" link' do
+      visit '/'
+      expect(page).to have_content 'Join with Facebook'
+    end
+
+    it 'can login with facebook' do
+      visit '/'
+      click_link 'Join with Facebook'
+      expect(page).to have_content 'Successfully authenticated from Facebook account.'
+    end
+
+    it 'when logged in cannot see "Join"/"Log in" links' do
+      visit '/'
+      click_link 'Join with Facebook'
+      expect(page).not_to have_content 'Join with Facebook'
+      expect(page).not_to have_content 'Join with Email'
+      expect(page).not_to have_content 'Log in'
+    end
+
+    it 'can logout' do
+      visit '/'
+      click_link 'Join with Facebook'
+      click_link 'Log out'
+      expect(page).to have_content 'Signed out successfully.'
     end
 
   end
