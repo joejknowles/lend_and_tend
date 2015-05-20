337db5f56037c141e5e789917a837841e4e643da
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-05-20 14:23:22 +0100

    Added initial static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
-  root 'static#index'
+  root to: 'static#index'
+
+  get '/about_us' => 'static#about_us'
+  get '/faq' => 'static#faq'
+  get '/terms' => 'static#terms'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
