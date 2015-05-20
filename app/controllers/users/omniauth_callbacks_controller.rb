67f95b900c789722c08d999c3e4311ccb03e77b3
Joseph Knowles <joejknowles@gmail.com>
2015-05-20 17:31:09 +0100

    merged

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect(@user,  event: :authentication)
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     else
-      flash[:error] = "Something went wrong for us, please try joining with your Email address instead!"
+      flash[:errors] = "Something went wrong for us, please try joining with your Email address instead!"
       redirect_to new_user_registration_url
     end
   end
