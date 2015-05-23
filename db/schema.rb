63068d6f8b870ae8ab32009fd3fa6f661f6328aa
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-05-23 19:46:55 +0100

    Patches now have use status - default free

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522144202) do
+ActiveRecord::Schema.define(version: 20150523182955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,13 +26,14 @@ ActiveRecord::Schema.define(version: 20150522144202) do
   end
 
   create_table "patches", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.string   "location"
     t.string   "patch_type"
     t.string   "duration"
     t.integer  "user_id"
     t.string   "description"
+    t.boolean  "in_use",      default: false
   end
 
   add_index "patches", ["user_id"], name: "index_patches_on_user_id", using: :btree
