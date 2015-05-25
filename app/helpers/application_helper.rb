98af3d486301190f1e5ef09943fb8e52f3aa2e11
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-05-25 16:00:12 +0100

    Customised the behaviour of will_paginate

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+
+  def pagination_links(collection, options = {})
+    options[:renderer] = PaginationHelper::LinkRenderer
+    options[:class] = 'pagination pagination-centered'
+    options[:inner_window] ||= 2
+    options[:outer_window] ||= 1
+    will_paginate collection, options
+  end
+
   #Below methods are to enable devise login form on none devise pages
 
   def resource_name
