533cb91e6e655c8a7a5aae0dca296c706bb1a5cd
Joseph Knowles <joejknowles@gmail.com>
2015-05-26 19:48:11 +0100

    homepage images fade in and out

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
   setInterval(function() {
     var divID = Math.floor((Math.random() * 14 ) + 1);
-    imageUrls.push($('#homeImg' + divID).css("background-image"));
-    $('#homeImg' + divID).css("background-image", imageUrls.shift())
-  }, 1000);
+    var $divToChange = $('#homeImg' + divID);
+    $divToChange.fadeOut(300, function() {
+      imageUrls.push($divToChange.css("background-image"));
+      $divToChange.css("background-image", imageUrls.shift());
+      $divToChange.fadeIn(300);
+    });
+
+  }, 3000);
 });
