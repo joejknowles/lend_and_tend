8d016c9a21d0736e261c78a49237c6b450c29289
Jade King <dev@jadeking.co.uk>
2015-05-27 11:19:30 +0100

    lower max image size

--- app/models/user_avatar.rb
+++ app/models/user_avatar.rb
@@ -1,6 +1,6 @@
 class UserAvatar < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image, styles: { medium: '250x250', thumb: '100x100' }, default_url: 'http://placehold.it/250x250'
+  has_attached_file :image, styles: { medium: '200x200', thumb: '100x100' }, default_url: 'http://placehold.it/250x250'
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
